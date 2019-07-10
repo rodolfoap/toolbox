d298cc05149ba24a7d37e77620e5e5713dfaf4e3
rodolfoap <rodolfoap@hotmail.com>
2019-07-10 16:09:29 +0200

    Changes 2019/07/10-16:09:29
    Change-Id: I72eb54345ba85a1d249ced6e18a2d1b8eaf59c86

---
+++ java/jni/HelloJNI.java
@@ -0,0 +1,11 @@
+public class HelloJNI {
+    static {
+        System.loadLibrary("hello");    // loads libhello.so
+    }
+
+    private native void sayHello(String name);
+
+    public static void main(String[] args) {
+        new HelloJNI().sayHello("Dave");
+    }
+}
