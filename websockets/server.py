72b6699dee44f3bf52c4598cd9571ac1f55f7e96
rodolfoap <rodolfoap@hotmail.com>
2019-08-29 09:08:27 +0200

    Changes 2019/08/29-09:08:27
    Change-Id: I3f1c16dd6efcb53fc71e9064eeb506e918fa772d

--- websockets/server.py
+++ websockets/server.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 
+from threading import Thread
+import time
+import subprocess
 from websocket_server import WebsocketServer
 
+lastclient=None
 def on_new_client(client, server):
+	global lastclient
+	lastclient=client
 	server.send_message_to_all("Hey all, a new client has joined us")
 	server.send_message(client, "Welcome, client "+str(client['id']))
 
@@ -13,8 +19,24 @@ def on_client_leave(client, server):
 def on_message_received(client, server, message):
 	print("Message from client[%d]: %s" % (client['id'], message))
 
+def threaded_function(arg):
+	global lastclient
+	while True:
+		if(lastclient):
+			bashCommand = "date +%s"
+			process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+			output, error = process.communicate()
+			server.send_message(lastclient, output)
+		time.sleep(1)
+
 server=WebsocketServer(8080, host='0.0.0.0')
 server.set_fn_new_client(on_new_client)
 server.set_fn_client_left(on_client_leave)
 server.set_fn_message_received(on_message_received)
+
+thread = Thread(target = threaded_function, args = (10, ))
+thread.start()
+#hread.join()
+
 server.run_forever()
+
