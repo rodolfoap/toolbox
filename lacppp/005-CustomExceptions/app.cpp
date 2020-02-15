f26027319a8beaa78a97366dc44e685de278bca8
RodolfoAP <rodolfoap@gmail.com>
2020-02-15 21:20:59 +0100

    Changes 2020/02/15-21:20:59
    Change-Id: I791153b2b6d9b57709003b619213071e518759f2

--- lacppp/005-CustomExceptions/app.cpp
+++ lacppp/005-CustomExceptions/app.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
 
+class MyException :public std::exception{
+public:
+	virtual const char* what() const throw(){
+		return "MY_ERROR";
+	}
+};
+class Test{
+public:
+	void genError(){
+		throw MyException();
+	}
+};
+
 int main(){
+	Test t;
+	try{
+		t.genError();
+	} catch(MyException &e){
+		std::cerr<<"[ERROR] "<< e.what() <<std::endl;
+	}
 	return 0;
 }
