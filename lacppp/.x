5e6754288439f470b8d628de11d3bddadef3f61b
RodolfoAP <rodolfoap@gmail.com>
2020-02-16 06:59:22 +0100

    Changes 2020/02/16-06:59:22
    Change-Id: Ia59628dc219a34cc26e42ef8598834804eb56ca9

--- lacppp/.x
+++ lacppp/.x
@@ -1,15 +1,16 @@
 gencppapp(){
-	cp -rvd 000-Model/ $1/
-	cd $1
+	FILE=$(ls vid/${1}*|sed 's:^vid/::;s/.mp4$//')
+	cp -rvd 000-Model/ $FILE
+	cd $FILE
 }
 
 case "$1" in
 '')
-	echo "  Usage: x [xxx-ProjectName]"
-	echo "Example: x 005-CustomExceptions"
+	echo "  Usage: x [NNN]"
+	echo "Example: x 005 # Will use 005-CustomExceptions"
 ;;
 *)
 	gencppapp $1
-	e
+#	e
 ;;
 esac
--- lacppp/007-WritingTextFiles/app.cpp
+++ lacppp/007-WritingTextFiles/app.cpp
@@ -2,9 +2,16 @@
 #include <fstream>
 
 int main(){
-	std::ofstream file_temp;
-	file_temp.open("file.temp");
+	/* Method 1 */
+	//std::ofstream file_temp;
+	//file_temp.open("file.temp");
+
+	/* Method 2 */
+	std::fstream file_temp;
+	file_temp.open("file.temp", std::ios::out);
+
 	if(!file_temp.is_open()) { std::cerr<<"[ERR]"<<std::endl; return 1; }
+
 	file_temp<<"some data"<<std::endl;
 	file_temp.close();
 	return 0;
