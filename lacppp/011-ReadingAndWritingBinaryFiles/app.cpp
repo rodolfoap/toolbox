6b57e6becd63b95a349152c85a9d464064320dd0
RodolfoAP <rodolfoap@gmail.com>
2020-02-19 03:35:21 +0100

    Changes 2020/02/19-03:35:21
    Change-Id: Ic62b8ac66cacd3b02d0383a32f4303567b0d39cc

--- lacppp/011-ReadingAndWritingBinaryFiles/app.cpp
+++ lacppp/011-ReadingAndWritingBinaryFiles/app.cpp
@@ -3,20 +3,47 @@
 
 #pragma pack(push, 1)
 struct Data {
-	std::string name;
+	// sizeof() rounds to 48 without #pragma
+	// sizeof() rounds to 44 with    #pragma
+	char name[32]; 	// 32
+	double weight; 	//  8
+	int age; 	//  4
+	// std::string name; Of course, not. Such is a pointer to the heap, not a space on the stack.
 };
 #pragma pack(pop)
 
 int main(){
-	Data data;
-	data.name="Test";
-	std::cerr<<sizeof(data)<<std::endl;
+	Data data = { "ABCDabcd", 77.44, 255 };
+	std::cerr<<"sizeof(struct): "<<sizeof(Data)<<std::endl;
 
+	/* Writing */
 	std::fstream file;
 	file.open("data.temp", std::ios::binary|std::ios::out);
 	if(file.is_open()){
-		file.write((char *)&data, sizeof(data));
+
+	// This will not work: write() expects a pointer to char[]
+	//	file.write(&data, sizeof(data));
+
+	// Fixed:
+	//	file.write((char *)&data, sizeof(data));
+
+	// Yet better:
+		file.write(reinterpret_cast<char *>(&data), sizeof(Data));
 		file.close();
+	}
+
+	/* Reading */
+	Data datx;
+	std::fstream filx;
+	filx.open("data.temp", std::ios::binary|std::ios::in);
+	if(filx.is_open()){
+		filx.read(reinterpret_cast<char *>(&datx), sizeof(Data));
+		std::cerr
+			<<"["<<datx.name
+			<<":"<<datx.weight
+			<<":"<<datx.age
+			<<"]"<<std::endl;
+		filx.close();
 	}
 	return 0;
 }
