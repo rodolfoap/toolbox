6b433dd601fde05e08bc32837c21e9baa49f0a1f
RodolfoAP <rodolfoap@gmail.com>
2020-02-20 02:46:08 +0100

    Changes 2020/02/20-02:46:08
    Change-Id: I178514e9c5266ddb6f6bcbdf759a641ee35e96d4

--- cpp/30-allStl/app.cpp
+++ cpp/30-allStl/app.cpp
@@ -11,10 +11,8 @@
 #include <string>
 #include <vector>
 
-using namespace std;
-
 struct Person {
-	string name;
+	std::string name;
 	int age;
 } p1, p2, p3;
 
@@ -31,108 +29,108 @@ bool compare_names(struct Person p1, struct Person p2) {
 bool way_to_sort(int i, int j) { return i > j; }
 
 int main() {
+	/*
+
 	// STACK
-	stack <string> distros; //Create a stack of strings.
-	distros.push("Ubuntu");  //Pushing elements into the stack.
+
+	// Create a std::stack of std::strings.
+	std::stack <std::string> distros;
+	//Pushing elements into the std::stack.
+	distros.push("Ubuntu");
 	distros.push("Mint");
-	cout << "Number of distros in the stack are " << distros.size() << endl;
-	cout << "Distro on the top is " << distros.top() << endl;
+	std::cout << "std::stack size(): " << distros.size() << std::endl;
+	std::cout << "std::stack  top(): " << distros.top() << std::endl;
 	distros.pop();
-	cout << "The top of the stack is now " << distros.top() << endl;
+	std::cout << "std::stack  top(): " << distros.top() << std::endl;
 
 	// VECTOR
-	vector <int> numbers;
-	if (numbers.empty()) { //check if the vector is empty?
-		cout << "The vector is empty :(" << endl;
-	}
-	for(int i=0; i<100; i+=10) { //Add some values to the vector
-		numbers.push_back(i);
-	}
-	cout << "Size of the vector is " << numbers.size() << endl;
-	// iterating over the vector, declaring the iterator
-	vector <int>::iterator it;
-	cout << "The vector contains: ";
-	for (it=numbers.begin(); it!=numbers.end(); it++) {
-		cout << "  " << *it;
-	}
+
+	std::vector <int> nvec;
+	// Vector is empty
+	std::cout << "std::vector empty() :" << (nvec.empty()?"TRUE":"FALSE") << std::endl;
+	std::cout << "std::vector  size() :" << nvec.size() << std::endl;
+	// Add values
+	for(int i=0; i<10; i++) { nvec.push_back(i); }
+	std::cout << "std::vector empty() :" << (nvec.empty()?"TRUE":"FALSE") << std::endl;
+	std::cout << "std::vector  size() :" << nvec.size() << std::endl;
+
+	// Iterating
+	std::vector <int>::iterator it;
+	std::cout<<"std::vector: "; for(it=nvec.begin(); it!=nvec.end(); it++) std::cout<<*it<<" "; std::cout<<std::endl;
 	// getting value at particular position
-	int position = 5;
-	cout<<"\nVector at position "<<position<<" contains "<<numbers.at(position)<<endl;
+	int pos = 5;
+	std::cout<<"std::vector("<<pos<<")=="<<nvec.at(pos)<<std::endl;
+
 	// deleting an element at position
-	numbers.erase(numbers.begin() + position);
-	cout<<"Vector at position "<<position<<" contains "<<numbers.at(position)<<endl;
-	// deleting a range of elements, first two elements
-	// NOTE: You may expect elements at 0, 1 and 2 to be deleted
-	// but index 2 is not inclusive.
-	numbers.erase(numbers.begin(), numbers.begin()+2);
-	cout << "The vector contains: ";
-	for (it=numbers.begin(); it!=numbers.end(); it++) {
-		cout << "  " << *it;
-	}
-	// Clearing the vector
-	numbers.clear();
-	if (numbers.empty()) {
-		cout << "\nThe vector is now empty again :(";
-	}
+	nvec.erase(nvec.begin()+pos);
+	std::cout<<"std::vector: "; for(it=nvec.begin(); it!=nvec.end(); it++) std::cout<<*it<<" "; std::cout<<std::endl;
+
+	// deleting range (elements 1 to 3)
+	// You may expect elements at 1, 2 and 3 to be deleted, but index 3 is not inclusive.
+	nvec.erase(nvec.begin()+1, nvec.begin()+3);
+	std::cout<<"std::vector: "; for(it=nvec.begin(); it!=nvec.end(); it++) std::cout<<*it<<" "; std::cout<<std::endl;
+
+	// Clearing the std::vector
+	nvec.clear();
+	std::cout << "std::vector empty() :" << (nvec.empty()?"TRUE":"FALSE") << std::endl;
 
 	// HASHMAP
-	// Declaration <key type, value type>
-	map <string, string> companies;
-	companies["Google"] = "Larry Page";
-	companies["Facebook"] = "Mark Zuckerberg";
-	// insertion can also be done as
-	companies.insert(pair<string, string> ("Xarvis Tech", "xarvis"));
-	// or
-	companies.insert(map<string,string>::value_type("Quora", "Adam D'Angelo"));
-	// or even
-	companies.insert(make_pair(string("Uber"), string("Travis Kalanick")));
+	// Declaration: <key_type, value_type>
+	std::map <std::string, std::string> hmap;
+	hmap["Google"] = "Larry Page";
+	hmap["Facebook"] = "Mark Zuckerberg";
+	// Several insertion mechanisms:
+	hmap.insert(std::pair<std::string, std::string> ("Xarvis Tech", "Xarvis Le Pen"));
+	hmap.insert(std::map<std::string,std::string>::value_type("Quora", "Adam D'Angelo"));
+	hmap.insert(std::make_pair(std::string("Uber"), std::string("Travis Kalanick")));
 	// Iterating the map
-	map<string, string>::iterator itz;
-	cout << "\n\nCompanies and founders" << endl;
-	for (itz=companies.begin(); itz!=companies.end(); itz++) {
-		cout << "Company: " << (*itz).first << "\t Founder: " << itz->second <<endl;
-	}
-	itz = companies.find("Google");
-	cout << itz->second;
-	// Check if key exists
-	// if ( m.find("f") == m.end() ) { cout<<"Not found"<<endl; } else { cout<<"Found"<<endl; }
+	std::map<std::string, std::string>::iterator m;
+	std::cout<<"std::map:"<< std::endl;
+	for(m=hmap.begin(); m!=hmap.end(); m++) std::cout<<"("<<(*m).first<< ", "<<m->second<<")"<<std::endl;
+	// Finding, verifying
+	m=hmap.find("Google");
+	if(m==hmap.end()){ std::cout<<"Not found"<<std::endl; } else { std::cout<<"Found: "<<m->second<<std::endl; }
+
+	*/
 
 	// LINKED LISTS
-	list<int> mylist;
-	list<int>::iterator it1,it2,itx;
-	// set some values:
-	for (int i=1; i<10; ++i) mylist.push_back(10*i);
-	// 10 20 30 40 50 60 70 80 90
-	it1 = it2 = mylist.begin(); // ^^
-	advance (it2,6);            // ^                 ^
-	++it1;                      //    ^              ^
-	it1 = mylist.erase (it1);   // 10 30 40 50 60 70 80 90
-	//    ^           ^
-	it2 = mylist.erase (it2);   // 10 30 40 50 60 80 90
-	//    ^           ^
-	++it1;                      //       ^        ^
-	--it2;                      //       ^     ^
-	mylist.erase (it1,it2);     // 10 30 60 80 90
-	cout << "\nmylist contains:";
-	for (itx=mylist.begin(); itx!=mylist.end(); ++itx)
-		cout << ' ' << *itx;
-	cout << '\n';
-	// NOTE: it1 still points to 40, and 60 is not deleted
-	cout << endl << *it1 << "\t" << *it2 <<endl;
-	// This will print an unexpected value
-	it1++;
-	cout << *it1;
-	cout << "\nmylist now contains:";
-	for (it1=mylist.begin(); it1!=mylist.end(); ++it1)
-		cout << ' ' << *it1;
-	cout << '\n';
 
+	std::list<int> llist;
+	std::list<int>::iterator it1,it2,itx;
+	// Add values: 0 10 20 30 40 50 60 70 80 90
+	for(int i=0; i<10; ++i) llist.push_back(10*i);
+	std::cout<<"std::list:"; for(itx=llist.begin(); itx!=llist.end(); ++itx) std::cout<<" "<<*itx; std::cout<<std::endl;
+
+	// Navigating, removing
+	it1=it2=llist.begin();	// At [0]
+	++it1;			// At [10]
+	int index=std::distance(llist.begin(), it1);
+	std::cout<<"std::list["<<index<<"]: "<<*it1<<std::endl;
+	it1=llist.erase(it1);	// Removes [10]
+	std::cout<<"std::list:"; for(itx=llist.begin(); itx!=llist.end(); ++itx) std::cout<<" "<<*itx; std::cout<<std::endl;
+	advance(it2,6);		// At [60]
+	std::cout<<"std::list["<<std::distance(llist.begin(), it2)<<"]: "<<*it2<<std::endl;
+	it2=llist.erase(it2);   // Removes [70]
+	std::cout<<"std::list:"; for(itx=llist.begin(); itx!=llist.end(); ++itx) std::cout<<" "<<*itx; std::cout<<std::endl;
+
+	// Removing range
+	++it1;
+	--it2;
+	std::cout<<"std::list[(it1)"<<std::distance(llist.begin(), it1)<<"]: "<<*it1<<std::endl;
+	std::cout<<"std::list[(it2)"<<std::distance(llist.begin(), it2)<<"]: "<<*it2<<std::endl;
+	llist.erase(it1,it2);     // 10 30 60 80 90
+	std::cout<<"std::list:"; for(itx=llist.begin(); itx!=llist.end(); ++itx) std::cout<<" "<<*itx; std::cout<<std::endl;
+	// it1 still points to 30, and 60 was not deleted
+	std::cout<<"it1: "<<*it1<<"(already deleted); it2: "<<*it2<<std::endl;
+	// This will print an unexpected value
+	std::cout << *it1++;
+/*
 	// HEAPS
 
 	// Creates a max heap
 	priority_queue <int> pq;
 	// To create a min heap instead, just uncomment the below line
-	// priority_queue <int, vector<int>, greater<int> > pq;
+	// priority_queue <int, std::vector<int>, greater<int> > pq;
 	pq.push(5);
 	pq.push(1);
 	pq.push(10);
@@ -140,7 +138,7 @@ int main() {
 	pq.push(20);
 	// Extracting items from the heap
 	while (!pq.empty()) {
-		cout << pq.top() << " ";
+		std::cout << pq.top() << " ";
 		pq.pop();
 	}
 	// creating heap from user defined objects
@@ -154,46 +152,47 @@ int main() {
 	// Initialize a min heap
 	// Note: We defined a comparator is_older in the beginning to
 	// compare the ages of two person.
-	priority_queue <struct Person, vector<struct Person>, is_older> mh;
+	priority_queue <struct Person, std::vector<struct Person>, is_older> mh;
 	mh.push(p1);
 	mh.push(p2);
 	mh.push(p3);
 	// Extracting items from the heap
 	while (!mh.empty()) {
 		struct Person p = mh.top();
-		cout << p.name << " ";
+		std::cout << p.name << " ";
 		mh.pop();
 	}
 
 	// SORTING
 
 	// The following list type initialization is only supported after C++11
-	//vector<int> int_vec = {56, 32, -43, 23, 12, 93, 132, -154};
+	//std::vector<int> int_vec = {56, 32, -43, 23, 12, 93, 132, -154};
 	// If the above style of initialization doesn't work, use the following one
 	static int arr[] = {56, 32, -43, 23, 12, 93, 132, -154};
 	int arr_len = sizeof(arr) / sizeof(arr[0]);
-	vector <int> int_vec(arr, arr + arr_len);
-	cout << endl;
+	std::vector <int> int_vec(arr, arr + arr_len);
+	std::cout << std::endl;
 	// Default: sort ascending
 	// sort(int_vec.begin(), int_vec.end());
 	// To sort in descending order:
 	// Do not include the () when you call wayToSort
 	// It must be passed as a function pointer or function object
 	sort(int_vec.begin(), int_vec.end(), way_to_sort);
-	for (vector <int>::iterator i = int_vec.begin(); i!=int_vec.end(); i++)
-		cout << *i << " ";
-	cout << endl;
+	for (std::vector <int>::iterator i = int_vec.begin(); i!=int_vec.end(); i++)
+		std::cout << *i << " ";
+	std::cout << std::endl;
 	// sorting the array
 	sort(arr, arr + arr_len);
 	for (int i=0; i < arr_len; i++) {
-		cout << arr[i] << " ";
+		std::cout << arr[i] << " ";
 	}
 	// Sorting user-defined objects
 	static struct Person persons[] = {p1, p2, p3};
 	sort(persons, persons+3, compare_names);
 	// This will printout the names in alphabetical order
 	for (int i=0; i < 3; i++) {
-		cout << persons[i].name << " ";
+		std::cout << persons[i].name << " ";
 	}
+	*/
 	return 0;
 }
