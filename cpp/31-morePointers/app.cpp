ec20a06ba1f076c648d1b01c0e28d7949158fae0
RodolfoAP <rodolfoap@ydor.org>
2020-02-24 18:37:51 +0100

    Changes 2020/02/24-18:37:51
    Change-Id: Id052ece3cb6bc8be75b567349c77b6e6bb4a7324

--- cpp/31-morePointers/app.cpp
+++ cpp/31-morePointers/app.cpp
@@ -1,15 +1,49 @@
 #include<iostream>
+/*
 
++-----------+
+|           |
+|   Heap    |
+|           |
++-----------+
+|           |
+|   Stack   |
+|           |
++-----------+
+|           |
+|  Static/  |
+|  Global   |
+|           |
++-----------+
+|           |
+| Code/Text |
+|           |
++-----------+
+
+*/
 int main(){
-	int a=22;
-	int* b=&a;
-	std::cerr << *b << std::endl; // 22
+	int i=22;		// Goes to the stack
+	char c[]="text";	// Goes to the stack
+	const char *p="char";	// p goes to the stack, "char" goes to the CODE segment (Read-Only)
+	std::cout<<std::endl;
 
-	char c[]="text";
-	char *d=c;
-	printf("PTR: c:%p, d:%p\n", c, d);
-	printf("ADR: c:%p, d:%p\n", &c, &d);
-	printf("STR: c:%s, d:%s\n", c, d);
-	std::cerr << d << std::endl; // 22
+	// &i and i are different: &i is an address, i is the content.
+	printf("&i=%d, i:%d, i:%d\n", &i, i, i);
+	// &i=1685897468, i:22, 	i:22
+
+	// &c == &c[0] == c; all are identic: all are AN ADDRESS.
+	// c cannot be the content: on strings, c points to the start,
+	// but the end is defined by \0.
+	printf("&c=%d, c:%d, c:%d, c:%s\n", &c, &c[0], c, c);
+	// &c=1685897463, c:1685897463,	c:1685897463, c:1685897463, c:text
+
+	// &p is an address; p is also an address (but different, in the CODE segment).
+	printf("&p=%d, p:%d, p:%s\n", &p, p, p);
+	// &p=1685897448, p:253079557, 	p:char
+
+	std::cout<<std::endl;
 	return 0;
 }
+
+
+
