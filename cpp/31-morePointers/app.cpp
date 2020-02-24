a79d68688d78d728fd4c0e66a872200fa3732a76
RodolfoAP <rodolfoap@gmail.com>
2020-02-24 06:17:24 +0100

    Changes 2020/02/24-06:17:24
    Change-Id: I203caf2d24466ba4a5bc119d4e4d4ee12102e9c7

---
+++ cpp/31-morePointers/app.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+int main(){
+	int a=22;
+	int* b=&a;
+	std::cerr << *b << std::endl; // 22
+
+	char c[]="char";
+	char *d=c;
+	printf("PTR: c:%p, d:%p\n", c, d);
+	printf("ADR: c:%p, d:%p\n", &c, &d);
+	printf("STR: c:%s, d:%s\n", c, d);
+	std::cerr << d << std::endl; // 22
+	return 0;
+}
