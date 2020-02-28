888aaab2083e87da1b404711d5ecec4477229d9b
RodolfoAP <rodolfoap@ydor.org>
2020-02-28 01:55:09 +0100

    Changes 2020/02/28-01:55:09
    Change-Id: If8762640b282fddc19fea11d4dd7f56d48501eb7

--- cpp/33-reverse/app.cpp
+++ cpp/33-reverse/app.cpp
@@ -2,13 +2,17 @@
 #include<string.h>
 
 int main(){
-	char n[50], r[50];
+	char n[64], r[64];
 	std::cout<<"Name: ";
 	std::cin>>n;		// john
-	int l=strlen(n); 	// 4 (without \0)
+
+	// sizeof(n) returns 	64;
+	// strlen(n) returns 	4 [j,o,h,n,\0] (does not count the \0)
+	int l=strlen(n); 	// 4
+
 	r[l]=0;			// ____0
 	for(int i=0; i<l; i++)
 		r[l-1-i]=n[i];	// r[3]=n[0]... etc.
-	std::cout<<r<<std::endl;
+	std::cout<<n<<std::endl<<r<<std::endl;
 	return 0;
 }
