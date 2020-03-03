44c4d49aaa0541878abecb080bac46837614a1fd
RodolfoAP <rodolfoap@gmail.com>
2020-03-03 03:43:24 +0100

    Changes 2020/03/03-03:43:24
    Change-Id: I41bd114ac8624bff1b34f79c650fb27dca5ac2e2

--- cpp/38-glade3-gtkmm/app.cpp
+++ cpp/38-glade3-gtkmm/app.cpp
@@ -6,52 +6,20 @@ class FrmMain: public Gtk::Window {
         Gtk::Button *display_button;
 	Glib::RefPtr<Gtk::Builder> builder;
 public:
-	FrmMain(): builder{Gtk::Builder::create_from_file("simple.glade")} {
+	FrmMain(): builder{Gtk::Builder::create_from_file("app.ui")} {
 		builder->get_widget<Gtk::Box>("cont", cont);
 	        builder->get_widget("display_label", display_label);
 	        builder->get_widget("display_button", display_button);
-		display_button->signal_clicked().connect( [this]() { display_label->set_text("Hello World"); });
+	        display_button->signal_clicked().connect(sigc::mem_fun(*this, &FrmMain::on_ok_button_clicked));
 		add(*cont);
-		//set_title("Simple Gtk::Builder demo");
-		//set_default_size(400, 400);
 		show_all();
 	}
+private:
+        void on_ok_button_clicked(){ display_label->set_text("Hello World"); }
 };
 
 int main(int argc, char *argv[]) {
-	auto app=Gtk::Application::create( argc, argv, "org.gtkmm.example.HelloApp");
+	auto app=Gtk::Application::create(argc, argv, "org.gtkmm.example.HelloApp");
 	FrmMain hw;
 	return app->run(hw);
 }
-
-/*
-class FrmMain : public Gtk::Window{
-protected:
-        Glib::RefPtr<Gtk::Builder> builder;
-        Gtk::Button *btnOk;
-        Gtk::Button *btnCancel;
-        Gtk::Label *lblNotice;
-public:
-	FrmMain(BaseObjectType* bot, const Glib::RefPtr<Gtk::Builder>& refg): Gtk::Window(bot), builder(refg){
-	        builder->get_widget("btnOk", btnOk);
-	        builder->get_widget("btnCancel", btnCancel);
-	        builder->get_widget("lblNotice",lblNotice);
-	        btnOk->signal_clicked().connect(sigc::mem_fun(*this, &FrmMain::on_ok_button_clicked));
-	        btnCancel->signal_clicked().connect(sigc::mem_fun(*this, &FrmMain::on_cancel_button_clicked));
-	}
-protected:
-        void on_ok_button_clicked(){ lblNotice->set_text("OK clicked"); }
-        void on_cancel_button_clicked(){ lblNotice->set_text("Cancel clicked"); }
-};
-
-int main(int argc, char **argv) {
-        cout << "Started" << endl;
-        Main kit(argc,argv);
-        Glib::RefPtr<Gtk::Builder> builder = Gtk::Builder::create_from_file("gui.glade");
-        FrmMain *frm = 0;
-        builder->get_widget_derived("frmMain", frm);
-        kit.run(*frm);
-        cout << "End" << endl;
-        return 0;
-}
-*/
