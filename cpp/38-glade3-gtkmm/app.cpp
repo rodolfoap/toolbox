0c7d33ca450eb3d9d899b913d4090f37549b4cd9
RodolfoAP <rodolfoap@gmail.com>
2020-03-03 04:27:32 +0100

    Changes 2020/03/03-04:27:32
    Change-Id: I99a9e2c0a05ce7d297d1c443d36c2807ce82c93a

--- cpp/38-glade3-gtkmm/app.cpp
+++ cpp/38-glade3-gtkmm/app.cpp
@@ -1,17 +1,17 @@
 #include <gtkmm.h>
 
 class FrmMain: public Gtk::Window {
-	Gtk::Box *cont;
+	Gtk::Box *gtkbox;
         Gtk::Label *display_label;
         Gtk::Button *display_button;
 	Glib::RefPtr<Gtk::Builder> builder;
 public:
 	FrmMain(): builder{Gtk::Builder::create_from_file("app.ui")} {
-		builder->get_widget<Gtk::Box>("cont", cont);
+		builder->get_widget<Gtk::Box>("main_gtk_box", gtkbox);
 	        builder->get_widget("display_label", display_label);
 	        builder->get_widget("display_button", display_button);
 	        display_button->signal_clicked().connect(sigc::mem_fun(*this, &FrmMain::on_ok_button_clicked));
-		add(*cont);
+		add(*gtkbox);
 		show_all();
 	}
 private:
@@ -19,7 +19,7 @@ private:
 };
 
 int main(int argc, char *argv[]) {
-	auto app=Gtk::Application::create(argc, argv, "org.gtkmm.example.HelloApp");
+	auto app=Gtk::Application::create(argc, argv);
 	FrmMain hw;
 	return app->run(hw);
 }
