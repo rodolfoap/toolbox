6a2130c45b560fe33053549a086ec0b38d5b98ce
RodolfoAP <rodolfoap@gmail.com>
2020-03-03 01:25:15 +0100

    Changes 2020/03/03-01:25:15
    Change-Id: I92038bf74e675e2514ad7778ed736e083470dc45

--- cpp/17-operators/app.cpp
+++ cpp/17-operators/app.cpp
@@ -4,12 +4,19 @@ class Vector{
 public:
 	float mag, dir;
 	Vector(float m, float d):mag(m), dir(d){}
-	// Comparison
-	bool operator==(const Vector& other)	const{ return (mag==other.mag && dir==other.dir); }
+	// Comparison 1
+	bool operator!=(const Vector& other)		const{ return !(mag==other.mag && dir==other.dir); }
+	// Comparison 2
+	std::string operator==(const Vector& other)	const{
+		if(mag==other.mag && dir==other.dir)
+			return "MATCH";
+		else
+			return "NOMATCH";
+	}
 	// Addition
-	Vector operator+(const Vector& other)	const{ return Vector(mag+other.mag, dir+other.dir); }
+	Vector operator+(const Vector& other)		const{ return Vector(mag+other.mag, dir+other.dir); }
 	// Addition method using the operator
-	Vector add(const Vector& other)		const{ return *this + other; }
+	Vector add(const Vector& other)			const{ return *this + other; }
 };
 // Overload <<
 std::ostream& operator<<(std::ostream& stream, const Vector& v){
@@ -18,15 +25,15 @@ std::ostream& operator<<(std::ostream& stream, const Vector& v){
 }
 
 int main(){
-	Vector a(7, 9);
-	Vector b(4, 4);
+	Vector a(7, 9), b(4, 4);
 	Vector c=a+b;
 	Vector d=b.add(a);
-	std::cerr << a << std::endl;
-	std::cerr << b << std::endl;
-	std::cerr << c << std::endl;
-	std::cerr << d << std::endl;
-	std::cerr << "C and D are " <<((c==d)?"equal":"different") << std::endl;
-	std::cerr << "A and D are " <<((a==d)?"equal":"different") << std::endl;
+
+	// operator== returns a std::string
+	std::cerr << c << ", " << d << ": " << (c==d) << std::endl;
+
+	// operator!= returns a bool
+	std::cerr << a << ", " << b << ": " << ((a!=b)?"NE":"EQ") << std::endl;
+
 	return 0;
 }
