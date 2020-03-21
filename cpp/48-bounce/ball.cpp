c81c538a1a16e1217b58fbcd298594f96ba3e456
RodolfoAP <rodolfoap@gmail.com>
2020-03-21 16:54:46 +0100

    Changes 2020/03/21-16:54:46
    Change-Id: I5520089fd8a13155734e040477d0b6b7007c5b19

--- cpp/48-bounce/ball.cpp
+++ cpp/48-bounce/ball.cpp
@@ -28,3 +28,28 @@ public:
 		app.draw(ball);
 	}
 };
+
+
+
+
+
+/*
+sf::Vector2f getSpriteSize(const sf::Sprite& ball) {
+	sf::IntRect size=ball.getTextureRect();
+	sf::Vector2f scale=ball.getScale();
+	return sf::Vector2f(size.width*scale.x, size.height*scale.y);
+}
+sf::Vector2f getSpriteCenter(const sf::Sprite& ball) {
+	sf::FloatRect aabb=ball.getGlobalBounds();
+	return sf::Vector2f(aabb.left+aabb.width/2.f, aabb.top+aabb.height/2.f);
+}
+
+bool collision(const sf::Sprite& ball1, const sf::Sprite& ball2) {
+	sf::Vector2f ball1Size=getSpriteSize(ball1);
+	sf::Vector2f ball2Size=getSpriteSize(ball2);
+	float radius1=(ball1Size.x+ball1Size.y)/4;
+	float radius2=(ball2Size.x+ball2Size.y)/4;
+	sf::Vector2f Distance=getSpriteCenter(ball1)-getSpriteCenter(ball2);
+	return (Distance.x*Distance.x+Distance.y*Distance.y<=(radius1+radius2)*(radius1+radius2));
+}
+*/
