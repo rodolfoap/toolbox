09df480762cef43819b9f8b76920d4fdd3e64976
RodolfoAP <rodolfo.alcazar@gtd.eu>
2020-03-29 03:56:49 +0200

    Changes 2020/03/29-03:56:49
    Change-Id: Ia4e5e3a1ae974748b08c86896f49659fb2a3291b

---
+++ cpp/50-VSCode/app.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#define LOG std::cerr<<">>> "<<__FILE__<<"["<<__LINE__<<"]:"<<__func__<<"();"<<std::endl;
+
+int main(){
+	std::cerr<<"Hello, World!"<<std::endl;
+	return 0;
+}
+/*
+
+- Start the app with "code ."
+- FILE/Preferences/Extensions: Install CMake Tools and C/C++ Extensions
+- Below, on the status bar, click on "Build" 
+- Run with [Shift][F5]
+*/
