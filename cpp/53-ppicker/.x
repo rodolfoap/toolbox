c73d13a9d218b0d8b0151f76b93c70c0c342a9da
RodolfoAP <rodolfo.alcazar@gtd.eu>
2020-04-04 18:14:52 +0200

    Changes 2020/04/04-18:14:52
    Change-Id: I810369937caccaba1ca52fa588facb8ee7b9601a

--- cpp/53-ppicker/.x
+++ cpp/53-ppicker/.x
@@ -1,5 +1,6 @@
 execute(){
 	./parsekml points_backyard.kml > points_backyard.dat
+	./app
 	./app crowd.jpg
 }
 debug(){
--- cpp/53-ppicker/app.cpp
+++ cpp/53-ppicker/app.cpp
@@ -2,9 +2,11 @@
 #include<SFML/Audio.hpp>
 #include<SFML/Graphics.hpp>
 #define LOG std::cerr<<">>> "<<__FILE__<<"["<<__LINE__<<"]:"<<__func__<<"();"<<std::endl;
+#define RADIUS 5
 
 sf::Event event;
 int main(int argc, char* argv[]) {
+	std::vector<sf::Vector2f> dots;
 	if(argc==1) { std::cerr<<"Usage: "<<argv[0]<<" [IMAGE_FILE]" <<std::endl; return 0; }
 
 	// Open window
@@ -16,8 +18,8 @@ int main(int argc, char* argv[]) {
 	sf::Sprite background(texture);
 
 	// Dot
-	sf::CircleShape dot(20);
-	dot.setFillColor(sf::Color(0, 96, 96));
+	sf::CircleShape dot(RADIUS);
+	dot.setFillColor(sf::Color(255, 0, 0));
 
 	// Screen setup
 	window.clear();
@@ -29,17 +31,24 @@ int main(int argc, char* argv[]) {
 		// Process events: waitEvent() is blocking, pollEvent() is not.
 		while (window.waitEvent(event)){
 			window.draw(background);
-			if(event.type==sf::Event::Resized){
-				std::cerr<<"r";
-				window.clear();
-			}
+			if(event.type==sf::Event::Resized){ window.clear(); }
 			if(event.type==sf::Event::MouseButtonReleased){
-				std::cerr<<".";
-				dot.setPosition(sf::Mouse::getPosition(window).x-20,
-						sf::Mouse::getPosition(window).y-20);
+				// See https://www.sfml-dev.org/tutorials/2.0/graphics-view.php#showing-more-when-the-window-is-resized
+				sf::Vector2i pixelPos = sf::Mouse::getPosition(window);
+				sf::Vector2f worldPos = window.mapPixelToCoords(pixelPos);
+				dots.push_back(worldPos);
+			}
+			for(int i=0; i<dots.size(); i++){
+				dot.setPosition(dots[i].x-RADIUS-1, dots[i].y-RADIUS-1);
 				window.draw(dot);
 			}
-			if(event.type==sf::Event::Closed){ std::cerr<<"c\n"; window.close(); }
+			if(event.type==sf::Event::Closed){
+				for(int i=0; i<dots.size(); i++){
+					dot.setPosition(dots[i].x-RADIUS-1, dots[i].y-RADIUS-1);
+					std::cerr<<dots[i].x<<":"<<dots[i].y<<std::endl;
+				}
+ 				window.close();
+			}
 			window.display();
 		}
 	}
