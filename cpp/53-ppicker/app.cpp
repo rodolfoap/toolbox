1b7ceed559103023b4ba97dde6cb10d486e23ed1
RodolfoAP <rodolfo.alcazar@gtd.eu>
2020-04-05 19:13:18 +0200

    Changes 2020/04/05-19:13:18
    Change-Id: I7869a4d325609dde1233bf9526d3541b65f85679

--- cpp/53-ppicker/app.cpp
+++ cpp/53-ppicker/app.cpp
@@ -8,27 +8,52 @@
 #define LOG std::cerr<<">>> "<<__FILE__<<"["<<__LINE__<<"]:"<<__func__<<"();"<<std::endl;
 #define RADIUS 4
 
+struct Window: public sf::RenderWindow {
+	std::vector<sf::Vector2f> dots;
+	sf::CircleShape dot;
+	sf::Texture texture;
+	sf::Sprite background;
+	Window(const int width, const int height, const std::string title, const char* bgndfile):
+		sf::RenderWindow(sf::VideoMode(width, height), title), dot(RADIUS) {
+
+		// Background
+		texture.loadFromFile(bgndfile);
+		background.setTexture(texture);
+		// Dot
+		dot.setFillColor(sf::Color(255, 0, 0));
+		// Screen setup
+		this->clear();
+		this->draw(background);
+		this->display();
+	}
+	void drawBackground(){
+		this->clear();
+		this->draw(background);
+	};
+	void addDot(const sf::Vector2f dot){
+		dots.push_back(dot);
+	}
+	void removeDot(){
+		if(dots.size()>0) dots.pop_back();
+	}
+	void drawDots(){
+		for(int i=0; i<dots.size(); i++) {
+			dot.setPosition(dots[i].x-RADIUS-1, dots[i].y-RADIUS-1);
+			this->draw(dot);
+		}
+	}
+};
+
 int main(int argc, char* argv[]) {
 	Form form;
 	sf::Event event;
-	std::vector<sf::Vector2f> dots;
 
 	if(argc==1) { std::cerr<<"Usage: "<<argv[0]<<" [IMAGE_FILE]" <<std::endl; return 0; }
 	std::string points=readFile("points.dat");
 
 	// Open window
-	sf::RenderWindow window(sf::VideoMode(800, 600), "PPicker");
-	// Background
-	sf::Texture texture;
-	if (!texture.loadFromFile(argv[1])) return EXIT_FAILURE;
-	sf::Sprite background(texture);
-	// Dot
-	sf::CircleShape dot(RADIUS);
-	dot.setFillColor(sf::Color(255, 0, 0));
-	// Screen setup
-	window.clear();
-	window.draw(background);
-	window.display();
+	Window window(800, 600, "PPicker", argv[1]);
+
 	// Game loop
 	while (window.isOpen()) {
 		// Process events: waitEvent() is blocking, pollEvent() is not.
@@ -40,24 +65,21 @@ int main(int argc, char* argv[]) {
 					// See https://www.sfml-dev.org/tutorials/2.0/graphics-view.php#showing-more-when-the-window-is-resized
 					sf::Vector2i pixelPos=sf::Mouse::getPosition(window);
 					sf::Vector2f worldPos=window.mapPixelToCoords(pixelPos);
-					dots.push_back(worldPos);
+					window.addDot(worldPos);
 				}
 				// Right click is removing last point
-				if(event.mouseButton.button==sf::Mouse::Right) { if(dots.size()>0) dots.pop_back(); }
+				if(event.mouseButton.button==sf::Mouse::Right) { window.removeDot(); }
 			}
 			// Close window or pressing Q
 			if((event.key.code==sf::Keyboard::Q)||(event.type==sf::Event::Closed)) {
-				for(int i=0; i<dots.size(); i++) form.addxy(dots[i].x, dots[i].y);
+				for(int i=0; i<window.dots.size(); i++) form.addxy(window.dots[i].x, window.dots[i].y);
 				form.render();
 				window.close();
 			}
 
 			// Drawing
-			window.draw(background);
-			for(int i=0; i<dots.size(); i++) {
-				dot.setPosition(dots[i].x-RADIUS-1, dots[i].y-RADIUS-1);
-				window.draw(dot);
-			}
+			window.drawBackground();
+			window.drawDots();
 			window.display();
 		}
 	}
