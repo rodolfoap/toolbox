2b0b5e9e269412b110a4109a0042a08a80ab6cf9
RodolfoAP <rodolfo.alcazar@gtd.eu>
2020-04-11 11:58:16 +0200

    Changes 2020/04/11-11:58:16
    Change-Id: I02954e14cae311633b74f3b18d34ca72d98cf769

--- testbench/menu_editor.py
+++ testbench/menu_editor.py
@@ -5,35 +5,23 @@ import re
 from os import listdir
 from menu_form import Menu_Form
 
-class Menu_Form(nps.Form):
-	def __init__(self, name):
-		self.fields={}
-		super(Menu_Form, self).__init__(name)
-
-	def create(self):
-		for tag in self.fields:
-			self.fields[tag].value=self.values[tag]
-
-	def add_field(self, tag, widgetClass, w_id=None, max_height=None, rely=None, relx=None, *args, **keywords):
-		self.fields[tag]=self.add(widgetClass, w_id, max_height, rely, relx, *args, **keywords)
-
-	def afterEditing(self):
-		self.parentApp.setNextForm(None)
-
 class Servc(nps.NPSAppManaged):
-	def onStart(self):
+	def main(self):
 		self.existing_tests=listdir('test_cases')
+		self.nfiles=len(self.existing_tests)
 		self.existing_tests.sort()
 		self.existing_tests.append('New Test:')
-		self.form=Menu_Form(name='Select Option')
-		self.form.add_field('TestFile', nps.TitleSelectOne, max_height=4, name="Test:",
-				values=self.existing_tests, value=0, scroll_exit=True)
-		self.form.add_field('TestName', nps.TitleText, name=" ", rely=len(self.existing_tests)+2, relx=7, value=0)
-		self.registerForm('MAIN', self.form)
+		self.form=nps.Form(name='Select Option')
+		self.test=self.form.add(nps.TitleSelectOne, max_height=4, name="Test:", values=self.existing_tests, value=0, scroll_exit=True)
+		self.newt=self.form.add(nps.TitleText, name=" ", rely=len(self.existing_tests)+2, relx=7, value=0)
+		self.form.edit()
 
 if __name__=='__main__':
 	s=Servc()
 	s.run()
-	print(s.existing_tests[s.form.fields['TestFile'].value[0]])
+	if s.test.value[0]==s.nfiles:
+		print(s.newt.value)
+	else:
+		print(s.existing_tests[s.test.value[0]])
 
 
