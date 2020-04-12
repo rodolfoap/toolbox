b8256ce9efaa4827218934416fb76b7a5f00adef
RodolfoAP <rodolfo.alcazar@gtd.eu>
2020-04-13 00:46:10 +0200

    Changes 2020/04/13-00:46:10
    Change-Id: Ic9536c27a80bf2f8f234c6e1dd906d088b1f63aa

--- testbench/run_test.bash
+++ testbench/run_test.bash
@@ -6,20 +6,30 @@ TESTNAME=$(echo ${TEST%.yaml}|sed 's:.*/::')
 LOGFILE=results/$TESTNAME.log
 RESULT=results/$TESTNAME.status
 
-echo [RUNTEST] $TEST started.
 LAUNCHER=$(yq r $TEST TestLauncherFilename)
 BASEDIR=$(dirname $LAUNCHER)
+BASENAM=$(basename $LAUNCHER)
+if [[ "$BASENAM" == manual* ]]; then TYPE=1; else TYPE=0; fi
+
+echo [RUNTEST] $TEST started.
 echo [RUNTEST] Launcher: $LAUNCHER
 echo [RUNTEST] STARTED: $TEST
-{
+case $TYPE in
+0) 	{ # Automatic test
 	pushd $BASEDIR &>/dev/null
 	echo
 	$LAUNCHER; [ $? == 0 ] && TEST_STATUS=PASS || TEST_STATUS=FAIL;
 	echo
 	popd &>/dev/null
 	echo TEST_STATUS=$TEST_STATUS > $RESULT
-} |& tee $LOGFILE
+	} |& tee $LOGFILE
+	grep "^[^ ]*=.*$" $LOGFILE >> $RESULT
+	echo $RESULT:
+	sed 's/^/\t/' $RESULT
+	echo
+;;
+1) 	# Manual test
+	$LAUNCHER;
+;;
+esac
 echo [RUNTEST] FINISHED: $TEST
-grep "^[^ ]*=.*$" $LOGFILE >> $RESULT
-echo $RESULT:
-sed 's/^/\t/' $RESULT
