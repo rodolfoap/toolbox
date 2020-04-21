649e641687c21a11176905bec4372ae1bbbcce95
RodolfoAP <rodolfoap@ydor.org>
2020-04-21 21:15:39 +0200

    Changes 2020/04/21-21:15:39
    Change-Id: I247ce9a0aa73d9cc6aa58dcac7e8c41edcb23935

--- cpp/48-bounce/ball.cpp
+++ cpp/48-bounce/ball.cpp
@@ -1,13 +1,42 @@
-#include<iostream>
-#include<exception>
-#include<SFML/Audio.hpp>
-#include<SFML/Graphics.hpp>
-#include<SFML/System.hpp>
-#include"rixmath.h"
+#include <iostream>
+#include <exception>
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include "rixmath.h"
 #define LOG std::cerr<<">>> "<<__FILE__<<"["<<__LINE__<<"]:"<<__func__<<"();"<<std::endl;
 
 int n=0;
 struct Ball {
+	sf::Texture texture;
+	sf::Sprite sprite;
+	float radius, minDistance;
+	sf::RenderWindow& win;
+	int width, height;
+	int id;
+	sf::Vector2f pos, speed;
+
+	// Constructors: only copies are initialized, check vector initialization method.
+	Ball(sf::RenderWindow& win, int width, int height): win(win), width(width), height(height), id(n++) {}
+	Ball(const Ball& b): win(b.win), width(b.width), height(b.height), id(n++) {
+		texture.loadFromFile("ball.png");
+		sprite=sf::Sprite(texture);
+		radius=sprite.getTextureRect().width/2+1;
+		minDistance=radius*2;
+		//if(id==1) {
+		//	pos=sf::Vector2f(200, 200);
+		//	speed=sf::Vector2f(5, 0);
+		//} else if (id==2) {
+		//	pos=sf::Vector2f(600, 220);
+		//	speed=sf::Vector2f(0, 0);
+		//} else {
+		float x=radius+1+rand()%(width -2*(int)radius);
+		float y=radius+1+rand()%(height-2*(int)radius);
+		pos=sf::Vector2f(x, y);
+		speed=sf::Vector2f(rand()%5-2, rand()%5-2);
+		//}
+	}
+
 	// Two balls colliding require a treatment
 	void checkBallColision(Ball& o){
 		// Positions, radius calculation
@@ -17,9 +46,7 @@ struct Ball {
 		// Collision!
 		if(distanceVectMag<minDistance) {
 			// Correct distance to avoid overlapping: move the other
-			sf::Vector2f distanceVectFixed=distanceVect;
-			sfm::Normalize(distanceVectFixed);
-			distanceVectFixed=distanceVectFixed*(minDistance-distanceVectMag);
+			sf::Vector2f distanceVectFixed=sfm::Normalized(distanceVect)*(minDistance-distanceVectMag);
 			o.pos+=distanceVectFixed;
 
 			// Rotate both speeds to emulate bouncing on a vertical wall
@@ -48,43 +75,4 @@ struct Ball {
 		pos+=speed;
 		sprite.setPosition(pos-sf::Vector2f(radius, radius));
 	}
-
-	sf::Texture texture;
-	sf::Sprite sprite;
-	float radius, minDistance;
-	sf::RenderWindow& win;
-	int width, height;
-	int id;
-	sf::Vector2f pos, speed;
-
-	// Constructors: only copies are initialized, check vector initialization method.
-	Ball(sf::RenderWindow& win, int width, int height): win(win), width(width), height(height), id(n++) {}
-	Ball(const Ball& b): win(b.win), width(b.width), height(b.height), id(n++) {
-		texture.loadFromFile("ball96.png");
-		sprite=sf::Sprite(texture);
-		radius=sprite.getTextureRect().width/2+1;
-		minDistance=radius*2;
-		if(id==1) {
-			pos=sf::Vector2f(200, 200);
-			speed=sf::Vector2f(5, 0);
-		} else if (id==2) {
-			pos=sf::Vector2f(600, 220);
-			speed=sf::Vector2f(0, 0);
-		} else {
-			pos=sf::Vector2f(rand()%width+1, rand()%height+1);
-			speed=sf::Vector2f(rand()%5-2, rand()%5-2);
-		}
-	}
-
 };
-
-/*
-bool collision(const sf::Sprite& ball1, const sf::Sprite& ball2) {
-	sf::Vector2f ball1Size=getSpriteSize(ball1);
-	sf::Vector2f ball2Size=getSpriteSize(ball2);
-	float radius1=(ball1Size.x+ball1Size.y)/4;
-	float radius2=(ball2Size.x+ball2Size.y)/4;
-	sf::Vector2f Distance=getSpriteCenter(ball1)-getSpriteCenter(ball2);
-	return (Distance.x*Distance.x+Distance.y*Distance.y<=(radius1+radius2)*(radius1+radius2));
-}
-*/
