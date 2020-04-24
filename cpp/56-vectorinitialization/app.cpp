b5bea3625def914e3f88a393f407770918d68ebf
RodolfoAP <rodolfo.alcazar@gtd.eu>
2020-04-24 06:47:21 +0200

    Changes 2020/04/24-06:47:21
    Change-Id: Iabd1786f4a97b29eed8db22ae418dd7dbbfe8d61

--- cpp/56-vectorinitialization/app.cpp
+++ cpp/56-vectorinitialization/app.cpp
@@ -20,9 +20,32 @@ int main(){
 	// Creates a vector with 3 balls
 	// Ball 0 dies here, after being copied as 1, 2, 3
 	std::vector<Ball> balls(3, Ball());
+	for(Ball& ball: balls) std::cerr<<"This is ball #"<<ball.pos<<std::endl;
 
-	for(Ball& ball: balls) {
-		std::cerr<<"This is ball #"<<ball.pos<<std::endl;
-	}
+	std::cerr<<"------------"<<std::endl;
+
+	// Better: use vector.emplace_back() instead of push_back():
+	// The problem of this is that the vector is doubled in size each
+	// time there's no space, and all elements are re-copied
+	std::vector<Ball> balls2;
+	balls2.emplace_back(Ball());
+	balls2.emplace_back(Ball());
+	balls2.emplace_back(Ball());
+	for(Ball& ball: balls2) std::cerr<<"This is ball #"<<ball.pos<<std::endl;
+
+	std::cerr<<"------------"<<std::endl;
+
+	// Better: use vector.emplace_back() instead of push_back():
+	// AND vector.reserve()
+	// The problem of this is that the vector is doubled in size each
+	// time there's no space, and all elements are re-copied
+	std::vector<Ball> balls3;
+	balls3.reserve(3);
+	// Notice the use of emplace_back() instead of emplace_back(Ball()):
+	// The constructor should allow this, otherwise, copies will be created.
+	balls3.emplace_back();
+	balls3.emplace_back();
+	balls3.emplace_back();
+	for(Ball& ball: balls3) std::cerr<<"This is ball #"<<ball.pos<<std::endl;
 	return 0;
 }
