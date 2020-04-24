293e1e7af349d29fd3b15bac31ce0a07754fedbf
RodolfoAP <rodolfo.alcazar@gtd.eu>
2020-04-24 17:04:32 +0200

    Changes 2020/04/24-17:04:32
    Change-Id: Ia85017003f4cd6a41a56009d43654f1f33236b6c

--- cpp/64-perspective/app.cpp
+++ cpp/64-perspective/app.cpp
@@ -2,10 +2,10 @@
 #include<SFML/Audio.hpp>
 #include<SFML/Graphics.hpp>
 #define LOG std::cerr<<">>> "<<__FILE__<<"["<<__LINE__<<"]:"<<__func__<<"();"<<std::endl;
-#define SHIFT_U +10
-#define SHIFT_V -500
+#define SHIFT_U +400
+#define SHIFT_V -150
 #define MULTI_U 800
-#define MULTI_V 20000
+#define MULTI_V 10000
 sf::Event e;
 int main() {
 	// Open window
@@ -18,11 +18,11 @@ int main() {
 		while (window.pollEvent(e)) if(e.key.code==sf::Keyboard::Q||e.type==sf::Event::Closed) window.close();
 		window.clear();
 		int d=1;
-		int z=40;
-		for(double x=0; x<800; x+=20) for(double y=0; y<600; y+=20) {
-			red.setPosition(x, y); window.draw(red);
-			double u=x * d/(y+800) *MULTI_U + SHIFT_U;
-			double v=z * d/(y+800) *MULTI_V + SHIFT_V;
+		int z=20;
+		for(double x=-400; x<400; x+=20) for(double y=-300; y<300; y+=20) {
+			red.setPosition(x+400, y+300); window.draw(red);
+			double u=x * d/(y+600) *MULTI_U + SHIFT_U;
+			double v=z * d/(y+600) *MULTI_V + SHIFT_V;
 			grn.setPosition(u, v); window.draw(grn);
 		}
 		window.display();
