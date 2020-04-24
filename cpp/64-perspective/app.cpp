ce74ab18593ea54f2688d1f8109f665559b3ebe2
RodolfoAP <rodolfo.alcazar@gtd.eu>
2020-04-24 17:35:05 +0200

    Changes 2020/04/24-17:35:05
    Change-Id: I1af09188033a6dd5eb79b39a196209de435c0800

--- cpp/64-perspective/app.cpp
+++ cpp/64-perspective/app.cpp
@@ -1,29 +1,42 @@
 #include<iostream>
 #include<SFML/Audio.hpp>
 #include<SFML/Graphics.hpp>
-#define LOG std::cerr<<">>> "<<__FILE__<<"["<<__LINE__<<"]:"<<__func__<<"();"<<std::endl;
-#define SHIFT_U +400
-#define SHIFT_V -150
-#define MULTI_U 800
-#define MULTI_V 10000
 sf::Event e;
+class Perspective{
+public:
+	// d: Focal distance, recommended 1;
+	// z: Camera altitude from the ground
+	// shiftH, shiftV: display shift
+	// multH, multV: multipliers
+	int d=1, z, shiftH, shiftV, multH, multV;
+
+	Perspective(int z, int shiftH, int shiftV, int multH, int multV):
+		z(z), shiftH(shiftH), shiftV(shiftV), multH(multH), multV(multV) {}
+
+	sf::Vector2f transformUV(double& x, double& y){
+		return sf::Vector2f(
+			x * d/(y+600) * multH + shiftH ,
+			z * d/(y+600) * multV + shiftV
+		);
+	}
+};
+
 int main() {
 	// Open window
 	sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
 	// Dot
-	sf::CircleShape red(1); red.setFillColor(sf::Color(255,   0, 0));
-	sf::CircleShape grn(1); grn.setFillColor(sf::Color(   0,255, 0));
-	// Game loop
+	sf::CircleShape red(1); red.setFillColor(sf::Color(255, 000, 000));
+	sf::CircleShape grn(1); grn.setFillColor(sf::Color(000, 255, 000));
+	// Values: (ground_alt, shiftH, shiftV, multH, multV);
+	Perspective per( 20, 		// Ground altitude
+			400,  -150,	// shift
+			400, 10000);	// multipliers
 	while (window.isOpen()) {
 		while (window.pollEvent(e)) if(e.key.code==sf::Keyboard::Q||e.type==sf::Event::Closed) window.close();
 		window.clear();
-		int d=1;
-		int z=20;
 		for(double x=-400; x<400; x+=20) for(double y=-300; y<300; y+=20) {
-			red.setPosition(x+400, y+300); window.draw(red);
-			double u=x * d/(y+600) *MULTI_U + SHIFT_U;
-			double v=z * d/(y+600) *MULTI_V + SHIFT_V;
-			grn.setPosition(u, v); window.draw(grn);
+			red.setPosition(x+400, y+300);          window.draw(red);
+			grn.setPosition(per.transformUV(x, y)); window.draw(grn);
 		}
 		window.display();
 	}
