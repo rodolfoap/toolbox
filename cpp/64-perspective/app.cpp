9c9ef6074281bd2f7edb38cf148fa9b6d234d896
RodolfoAP <rodolfo.alcazar@gtd.eu>
2020-04-24 18:11:45 +0200

    Changes 2020/04/24-18:11:45
    Change-Id: Ib7e5dd6672254b12df10e9e0d5688f4df74fadd9

--- cpp/64-perspective/app.cpp
+++ cpp/64-perspective/app.cpp
@@ -1,33 +1,6 @@
-#include<iostream>
-#include<SFML/Audio.hpp>
-#include<SFML/Graphics.hpp>
-#include"homog2d.hpp"
+#include <SFML/Graphics.hpp>
+#include "perspective.cpp"
 sf::Event e;
-class Perspective{
-public:
-	// d: Focal distance, recommended 1
-	// z: Camera altitude from the ground
-	// shiftH, shiftV: display shift
-	// multH, multV: multipliers
-	int d=1, z, shiftH, shiftV, multH, multV;
-	homog2d::Homogr h;
-
-	Perspective(int z, int shiftH, int shiftV, int multH, int multV):
-		z(z), shiftH(shiftH), shiftV(shiftV), multH(multH), multV(multV) { }
-
-	sf::Vector2f transformUV(double& a, double& b){
-		h.setRotation(3.14/1.8); // 30deg
-		h.addTranslation(0, 0);
-		homog2d::Point2d p1(a, b);
-		homog2d::Point2d p2 = h * p1;
-		double x=p2.getX();
-		double y=p2.getY();
-		double u=x * d/(y+400) * multH + shiftH;
-		double v=z * d/(y+400) * multV + shiftV;
-		h.clear();
-		return sf::Vector2f(u, v);
-	}
-};
 
 int main() {
 	// Open window
---
+++ cpp/64-perspective/perspective.cpp
@@ -0,0 +1,27 @@
+#include <SFML/Graphics.hpp>
+#include "homog2d.hpp"
+class Perspective{
+public:
+	// d: Focal distance, recommended 1
+	// z: Camera altitude from the ground
+	// shiftH, shiftV: display shift
+	// multH, multV: multipliers
+	int d=1, z, shiftH, shiftV, multH, multV;
+	homog2d::Homogr h;
+
+	Perspective(int z, int shiftH, int shiftV, int multH, int multV):
+		z(z), shiftH(shiftH), shiftV(shiftV), multH(multH), multV(multV) { }
+
+	sf::Vector2f transformUV(double& a, double& b){
+		h.setRotation(110*3.1416/180);
+		h.addTranslation(0, 0);
+		homog2d::Point2d p1(a, b);
+		homog2d::Point2d p2 = h * p1;
+		double x=p2.getX();
+		double y=p2.getY();
+		double u=x * d/(y+400) * multH + shiftH;
+		double v=z * d/(y+400) * multV + shiftV;
+		h.clear();
+		return sf::Vector2f(u, v);
+	}
+};
