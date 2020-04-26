792f731ff74e59c75f8b75a22903c4152c9b4110
RodolfoAP <rodolfoap@gmail.com>
2020-04-26 09:59:54 +0200

    Changes 2020/04/26-09:59:54
    Change-Id: Id44d6e9fa5ea40c383f85322889de28081d24d26

--- cpp/48-bounce/app.cpp
+++ cpp/48-bounce/app.cpp
@@ -2,17 +2,18 @@
 #include <SFML/System.hpp>
 #include "ball.cpp"
 #define LOG std::cerr<<">>> "<<__FILE__<<"["<<__LINE__<<"]:"<<__func__<<"();"<<std::endl;
-#define BALLS_QUANTITY 10
+#define BALLS_QUANTITY 100
 #define WIDTH 1278
 #define HEIGHT 749
 sf::Event e;
 
 int main() {
 	// Window
-	sf::RenderWindow win(sf::VideoMode(WIDTH, HEIGHT), "SFML window", sf::Style::Titlebar||sf::Style::Close);
+	sf::RenderWindow win(sf::VideoMode(WIDTH, HEIGHT), "Bouncing", sf::Style::Titlebar||sf::Style::Close);
 	win.setPosition(sf::Vector2i(0, 0));
 	win.setFramerateLimit(60);
-	std::vector<Ball> balls(BALLS_QUANTITY, Ball(win, WIDTH, HEIGHT));
+	Ball balls[BALLS_QUANTITY];
+	for(int a=0; a<BALLS_QUANTITY; a++){ balls[a].init(WIDTH, HEIGHT); }
 
 	// Loop
 	srand(time(0));
@@ -21,13 +22,13 @@ int main() {
 		while (win.pollEvent(e)) if(e.key.code==sf::Keyboard::Q||e.type==sf::Event::Closed) win.close();
 		// Drawing
 		win.clear();
-		for (std::vector<Ball>::iterator ball=balls.begin(); ball!=balls.end(); ++ball) {
-			ball->update();
+		for(int a=0; a<BALLS_QUANTITY; a++){ 
+			balls[a].update();
 			// Double loop for interactions: 1-2, 1-3, 1-4, 2-3, 2-4, 3-4 (if 4 objects)
-			for (std::vector<Ball>::iterator ball2=ball+1; ball2!=balls.end(); ++ball2) ball->checkBallColision(*ball2);
-			ball->checkBorderColision();
-			ball->move();
-			win.draw(ball->sprite);
+			for(int b=a+1; b<BALLS_QUANTITY; b++) balls[a].checkBallColision(balls[b]);
+			balls[a].checkBorderColision();
+			balls[a].move();
+			win.draw(balls[a].sprite);
 		}
 		win.display();
 	}
--- cpp/48-bounce/ball.cpp
+++ cpp/48-bounce/ball.cpp
@@ -10,19 +10,18 @@ struct Ball {
 	sf::Texture texture;
 	sf::Sprite sprite;
 	float radius, minDistance;
-	sf::RenderWindow& win;
 	int width, height;
 	sf::Vector2f pos, speed;
 
-	// Constructors: only copies are initialized, check vector initialization method.
-	Ball(sf::RenderWindow& win, int width, int height): win(win), width(width), height(height) {}
-	Ball(const Ball& b): win(b.win), width(b.width), height(b.height) {
+	// Initialize
+	void init(int w, int h) {
+		width=w; height=h;
 		texture.loadFromFile("ball48.png");
 		sprite=sf::Sprite(texture);
 		radius=sprite.getTextureRect().width/2;
 		minDistance=radius*2;
 		pos=sf::Vector2f(radius+1+rand()%(width -2*(int)radius), radius+1+rand()%(height-2*(int)radius));
-		speed=sf::Vector2f(rand()%20-10, rand()%20-10);
+		speed=sf::Vector2f(rand()%10-5, rand()%10-5);
 	}
 
 	// Two balls colliding require a treatment
