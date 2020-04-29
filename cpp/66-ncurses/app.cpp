a380e3813e82de8bc2e1affe6f797bb356a45132
RodolfoAP <rodolfo.alcazar@gtd.eu>
2020-04-29 08:35:55 +0200

    Changes 2020/04/29-08:35:55
    Change-Id: Id2e7fd6e3734c35a9130602d0e39daf69e963ff0

--- cpp/66-ncurses/app.cpp
+++ cpp/66-ncurses/app.cpp
@@ -5,40 +5,43 @@
 void update(char c) {
 	int x, y;
 	getmaxyx(stdscr, y, x);
-	mvprintw(y/2, x/2,"Hello, World! %d", c);
+	mvprintw(y/2, x/2,"Hello, World! %d", c); // moves and print
 	refresh();
 }
 
 int main(){
-	initscr();
-	noecho();
+	initscr(); // mandatory
+	noecho(); // avoid echo
 	int c;
 	attron(A_BOLD);
 	attron(A_REVERSE);
-	curs_set(1);
-	start_color();
-	use_default_colors();
-	init_pair(1, COLOR_GREEN, COLOR_BLACK);
-	init_pair(2, COLOR_CYAN, COLOR_BLUE);
+	curs_set(1); // shows (1) or hides (0) the cursor
+//	start_color(); // starts the modules color
+	use_default_colors(); // ncurses has horrible colors, this uses the users' default... more or less.
+	init_pair(1, COLOR_GREEN, COLOR_BLACK); // id, foreground, background
 	attron(COLOR_PAIR(1));
 
 	// main loop, close with q;
-	update(0);
-	while((c=getch())!=113) update(c);
+	update(0); // see the function above
+	while((c=getch())!=113) update(c); // exit with 'q'
 
 	// Bye!
-	//clear();
-	WINDOW* win=newwin(10, 20, 10, 10);
-	refresh();
-	//wbkgd(win, COLOR_PAIR(2));
-	//wmove(win, getmaxy(win)/2, getmaxx(win)/2);
-	box(win, 0, 0);
-//	wmove(win, 5, 5);
-//	wprintw(win, "Bye!");
-	//refresh();
-	wrefresh(win);
-getch();
-//	usleep(1000000);
+	clear();
+	WINDOW* win=newwin(5, 20, 0, 0);
+	refresh(); // required, to show the window
+	box(win, 0, 0); // draws a box
+	wmove(win, 2, 7); // moves relative to the window
+	wprintw(win, "Bye!"); // prints on the window
+	wrefresh(win); // updates window
+	getch();
+
+	for(int i=1; i<40; i++) {
+		clear(); // clears memory
+		refresh(); // updates screen
+		mvwin(win, i, i*2); // moves window
+		wrefresh(win); // updates window
+		usleep(30000); // 30ms
+	}
 
 	endwin();
 	return 0;
