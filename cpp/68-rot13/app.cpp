76a5ee2982c3166c2681aa1439536e026ed15ae3
RodolfoAP <rodolfo.alcazar@gtd.eu>
2020-04-30 19:40:08 +0200

    Changes 2020/04/30-19:40:08
    Change-Id: I52b2f3fb4a564e472faf41db3a6f48c3619b97d5

---
+++ cpp/68-rot13/app.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <unistd.h>
+
+void rot13(char* t) {
+	for(int i=0; t[i]!=0; i++){
+		if(t[i]>='a'&&t[i]<='z') t[i]=(((t[i]-'a')+13)%26)+'a';
+		if(t[i]>='A'&&t[i]<='Z') t[i]=(((t[i]-'A')+13)%26)+'A';
+	}
+}
+
+int main(){
+	char c, t[4096];
+	int n=0;
+
+	while(read(STDIN_FILENO, &c, 1)>0) t[n++]=c; t[n]=0;
+
+	rot13(t);
+	printf("%s\n", t);
+
+	return 0;
+}
