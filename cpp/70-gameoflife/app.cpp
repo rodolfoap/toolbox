dca24a9440ed179ff445e79ad15e2c5b35539416
RodolfoAP <rodolfoap@gmail.com>
2020-05-08 00:49:22 +0200

    Changes 2020/05/08-00:49:22
    Change-Id: I6540d9a9784a83cffbb9c8a88a52eee4dc602ab7

--- cpp/70-gameoflife/app.cpp
+++ cpp/70-gameoflife/app.cpp
@@ -1,11 +1,34 @@
 #include <iostream>
 #include <fstream>
 #include <string.h>
+#include <unistd.h>
+#define B(p, q)(b[y+p][x+q]=='#')
 int main(int n, char**v) {
 	int col=0, row=0;
 	char b[128][256];
 	std::ifstream f(v[1]);
 	while(f>>b[row++]);
-	for(n=0; n<row; ++n) { std::cerr<<b[n];	std::cerr<<std::endl; }
-	std::cerr<<strlen(b[0])<<std::endl;
+	col=strlen(b[0]);
+	while(1) {
+		int i[128][256]{0};
+		for(int y=0; y<row; ++y) {
+ 		       	for(int x=0; x<col; ++x) {
+				i[y][x]=B(-1,-1)+B(-1,0)+B(-1,1)+B(0,-1)+B(0,1)+B(1,-1)+B(1,0)+B(1,1);
+				std::cerr<<b[y][x];
+			} std::cerr<<std::endl;
+		} std::cerr<<std::endl;
+
+		for(int y=0; y<row; ++y) {
+			for(int x=0; x<col; ++x) {
+				// Apply life rules
+				switch(i[y][x]) {
+					case 3: b[y][x]='#'; break;
+					case 2: break;
+					default: b[y][x]='.';
+				}
+				std::cerr<<b[y][x];
+			} std::cerr<<std::endl;
+		} std::cerr<<std::endl;
+		usleep(200000);
+	}
 }
