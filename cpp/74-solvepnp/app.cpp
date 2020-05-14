f111a4f71b3dd001ce2a1d9cf053a8817ce308df
RodolfoAP <rodolfo.alcazar@gtd.eu>
2020-05-15 01:05:14 +0200

    Changes 2020/05/15-01:05:14
    Change-Id: I1d18cb51d500fd67c529c992a891baf8d370d91c

--- cpp/74-solvepnp/app.cpp
+++ cpp/74-solvepnp/app.cpp
@@ -14,10 +14,10 @@ int main() {
 	std::cout << "distCoeffs:\n" << distCoeffs << std::endl;
 
 	cv::Mat cameraMatrix = cv::Mat::eye(3, 3, CV_64F);
-	cameraMatrix.at<double>(0, 0) =   1.;	// fx
-	cameraMatrix.at<double>(1, 1) =   1.;	// fy
-	cameraMatrix.at<double>(0, 2) = 400.;	// cx
-	cameraMatrix.at<double>(1, 2) = 300.;	// cy
+	cameraMatrix.at<double>(0, 0) = 100.;	// fx, in pixels
+	cameraMatrix.at<double>(1, 1) = 100.;	// fy, in pixels
+	cameraMatrix.at<double>(0, 2) = 400.;	// cx, in pixels
+	cameraMatrix.at<double>(1, 2) = 300.;	// cy, in pixels
 	// | fx  0  cx |
 	// | 0  fy  cy |
 	// | 0   0   1 |
@@ -26,29 +26,30 @@ int main() {
 	std::vector<cv::Point2f> imagePoints;
 	std::vector<cv::Point3f> objectPoints;
 
-	//img points are green dots in the picture
-	imagePoints.push_back(cv::Point2f(271.,109.));
-	imagePoints.push_back(cv::Point2f(65.,208.));
-	imagePoints.push_back(cv::Point2f(334.,459.));
-	imagePoints.push_back(cv::Point2f(600.,225.));
+	// image points
+	imagePoints.push_back(cv::Point2f( 29.,  36.));
+	imagePoints.push_back(cv::Point2f(744.,  33.));
+	imagePoints.push_back(cv::Point2f( 29., 545.));
+	imagePoints.push_back(cv::Point2f(793., 490.));
 
-	//object points are measured in millimeters because calibration is done in mm also
-	objectPoints.push_back(cv::Point3f(0., 0., 0.));
-	objectPoints.push_back(cv::Point3f(-511.,2181.,0.));
-	objectPoints.push_back(cv::Point3f(-3574.,2354.,0.));
-	objectPoints.push_back(cv::Point3f(-3400.,0.,0.));
+	//object points are measured in cm
+	objectPoints.push_back(cv::Point3f(   0., 100., 0.));
+	objectPoints.push_back(cv::Point3f( 600.,   0., 0.));
+	objectPoints.push_back(cv::Point3f( 200., 700., 0.));
+	objectPoints.push_back(cv::Point3f( 600., 600., 0.));
+
 	cv::Mat rvec(1,3,cv::DataType<double>::type);
 	cv::Mat tvec(1,3,cv::DataType<double>::type);
 	cv::Mat rotationMatrix(3,3,cv::DataType<double>::type);
 	cv::solvePnP(objectPoints, imagePoints, cameraMatrix, distCoeffs, rvec, tvec);
 	cv::Rodrigues(rvec,rotationMatrix);
 
-	cv::Mat uvPoint = (cv::Mat_<double>(3,1) << 363, 222, 1); // u = 363, v = 222, got this point using mouse callback
+	cv::Mat uvPoint = (cv::Mat_<double>(3, 1) << 320, 253, 1); // u, v
 
 	cv::Mat leftSideMat  = rotationMatrix.inv() * cameraMatrix.inv() * uvPoint;
 	cv::Mat rightSideMat = rotationMatrix.inv() * tvec;
 
-	double s = (285 + rightSideMat.at<double>(2,0))/leftSideMat.at<double>(2,0);
+	double s = (200 + rightSideMat.at<double>(2,0))/leftSideMat.at<double>(2,0);
 	//285 represents the height Zconst
 
 	std::cout << "P = " << rotationMatrix.inv() * (s * cameraMatrix.inv() * uvPoint - tvec) << std::endl;
