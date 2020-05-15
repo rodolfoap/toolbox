7b5e96d809ead511a2ee4dff90f749793acf6dee
RodolfoAP <rodolfoap@gmail.com>
2020-05-15 23:13:18 +0200

    Changes 2020/05/15-23:13:18
    Change-Id: I39d4ab6e565ffdc494783cdd24ea9314d41261ec

---
+++ opencv/tilhub/test.cpp
@@ -0,0 +1,23 @@
+#include <opencv2/opencv.hpp>
+#include <string>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#define X 6			//number of vertices in a row of white blocks or number of white blocks in a rows*2
+#define Y 4			//number of vertics in a column of white blocks or number of white blocks in a column*2
+using namespace std;
+using namespace cv;
+
+Size boardSize(X,Y);
+int main() {
+	for (int i = 1; i <= 43; i++) {
+		string path_chessboards = "chessboards/"+to_string(i)+".jpg";
+		Mat image = imread(path_chessboards);
+		vector<Point2f> imageCorners;
+		bool found = findChessboardCorners(image, boardSize, imageCorners);
+		drawChessboardCorners(image, boardSize, imageCorners, found);
+		imageCorners.clear();
+		imshow("Test", image);
+		waitKey(1000);
+	}
+	return 0;
+}
